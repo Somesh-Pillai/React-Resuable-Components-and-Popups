3e6680da785088d29811e49aadb3c6e31ea0d13b
Somesh <imsomeshp@gmail.com>
2023-01-30 17:17:26 +0530

    [somesh] Add Popups

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Popup from "./components/Popup";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [buttonPopup, setButtonPopup] = useState(false);
+  const [triggerPopup, setTriggerPopup] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTriggerPopup(true);
+    }, 5000);
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <h1>React Popups!!</h1>
+        <br />
+        <button onClick={() => setButtonPopup(true)}>Show Popup</button>
+        <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+          <h3> Popup </h3>
+          <p>Popup from click button</p>
+        </Popup>
+
+        <Popup trigger={triggerPopup} setTrigger={setTriggerPopup}>
+          <h3> Popup tiggered </h3>
+          <p>Popup from trigger</p>
+        </Popup>
+      </main>
     </div>
   );
 }
